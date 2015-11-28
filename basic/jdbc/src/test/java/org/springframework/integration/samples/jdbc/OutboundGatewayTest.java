6968edb71c32e318324eb4a74421cd173b8f1bb4
Gary Russell <grussell@pivotal.io>
2015-11-28 12:25:54 -0500

    SO33920014 - retry/recovery polled adapter

--- basic/jdbc/src/test/java/org/springframework/integration/samples/jdbc/OutboundGatewayTest.java
+++ basic/jdbc/src/test/java/org/springframework/integration/samples/jdbc/OutboundGatewayTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,16 +15,24 @@
  */
 package org.springframework.integration.samples.jdbc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
 import java.util.Calendar;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.samples.jdbc.domain.Gender;
 import org.springframework.integration.samples.jdbc.domain.Person;
 import org.springframework.integration.samples.jdbc.service.PersonService;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessagingException;
 
 /**
  * The test class for jdbc outbound gateway
@@ -33,11 +41,11 @@ import org.springframework.integration.samples.jdbc.service.PersonService;
  */
 public class OutboundGatewayTest {
 
-	private Logger logger = Logger.getLogger(OutboundGatewayTest.class);	
-	
+	private final Logger logger = Logger.getLogger(OutboundGatewayTest.class);
+
 	@Test
-	public void insertPersonRecord() {	
-		ApplicationContext context
+	public void insertPersonRecord() {
+		ConfigurableApplicationContext context
         = new ClassPathXmlApplicationContext("/META-INF/spring/integration/spring-integration-context.xml");
 		PersonService service = context.getBean(PersonService.class);
 		logger.info("Creating person Instance");
@@ -48,8 +56,23 @@ public class OutboundGatewayTest {
 		person.setName("Name Of The Person");
 		person.setGender(Gender.MALE);
 		person = service.createPerson(person);
+
+		person.setName("Name Of The Second Person");
+		person = service.createPerson(person);
 		Assert.assertNotNull("Expected a non null instance of Person, got null", person);
 		logger.info("\n\tGenerated person with id: " + person.getPersonId() + ", with name: " + person.getName());
+
+		QueueChannel errors = context.getBean("errors", QueueChannel.class);
+		Message<?> received = errors.receive(10000);
+		assertNotNull(received);
+		assertThat(((MessagingException) received.getPayload()).getFailedMessage().toString(),
+				containsString("NAME OF THE PERSON"));
+		received = errors.receive(10000);
+		assertNotNull(received);
+		assertThat(((MessagingException) received.getPayload()).getFailedMessage().toString(),
+				containsString("NAME OF THE SECOND PERSON"));
+		logger.info("\n\tRetry and Error Recovery worked as expected");
+		context.close();
 	}
-	
+
 }
